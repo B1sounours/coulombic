a5788cb95cd9390b5f986f42ce5aedb9d16d5a18
Zulban <stuart.a.spence@gmail.com>
2015-05-23 04:27:23 -0400

    added random offset option

--- Assets/Scripts/Generic/CloneGenerator.cs
+++ Assets/Scripts/Generic/CloneGenerator.cs
@@ -4,9 +4,10 @@ using System.Collections.Generic;
 
 public class CloneGenerator : MonoBehaviour
 {
-    public int rowCount=1;
+    public int rowCount = 1;
     public Vector3 direction;
     public Vector3 direction2;
+    public Vector3 randomOffset;
 
     private List<Vector3> GetPoints()
     {
@@ -16,15 +17,26 @@ public class CloneGenerator : MonoBehaviour
             if (direction2.sqrMagnitude > 0)
             {
                 for (int j = 0; j < rowCount; j++)
-                    points.Add(new Vector3(direction.x * i + direction2.x * j, direction.y * i + direction2.y * j, direction.z * i + direction2.z * j));
+                {
+                    Vector3 newPoint = new Vector3(direction.x * i + direction2.x * j, direction.y * i + direction2.y * j, direction.z * i + direction2.z * j);
+                    points.Add(newPoint + GetRandomOffset());
+                }
             }
             else
             {
-                points.Add(new Vector3(direction.x * i, direction.y * i, direction.z * i));
+                Vector3 newPoint =new Vector3(direction.x * i, direction.y * i, direction.z * i);
+                points.Add(newPoint + GetRandomOffset());
             }
         }
         points.RemoveAt(0);
         return points;
+    }
+
+    private Vector3 GetRandomOffset()
+    {
+        return new Vector3(Random.Range(-randomOffset.x, randomOffset.x),
+            Random.Range(-randomOffset.y, randomOffset.y),
+            Random.Range(-randomOffset.z, randomOffset.z));
     }
 
     void Start()
@@ -43,7 +55,7 @@ public class CloneGenerator : MonoBehaviour
     {
         Gizmos.color = Color.red;
         if (GetComponent<ChargedObject>() != null && GetComponent<ChargedObject>().charge > 0)
-            Gizmos.color = Color.green;        
+            Gizmos.color = Color.green;
 
         foreach (Vector3 point in GetPoints())
             Gizmos.DrawSphere(gameObject.transform.position + point, 0.2f);
